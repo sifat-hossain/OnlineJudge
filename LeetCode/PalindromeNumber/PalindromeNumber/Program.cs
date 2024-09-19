9edd1db342928f5b1cd8078478f74153ea9321ad
Sifat Hossain <sifat1258@gmail.com>
2024-09-20 00:14:35 +0600

    Check the number is palindrome or not

--- LeetCode/PalindromeNumber/PalindromeNumber/Program.cs
+++ LeetCode/PalindromeNumber/PalindromeNumber/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        int reverse = 0;
+        int val = x;
+        if (val < 0) return false;
+        else
+        {
+            while (val != 0)
+            {
+                reverse = 10 * reverse + (val % 10);
+                val = val / 10;
+            }
+            if (reverse == x)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        int x = 0;
+        x = int.Parse(Console.ReadLine());
+        Console.WriteLine(s.IsPalindrome(x));
+        Console.ReadLine();
+    }
+}
