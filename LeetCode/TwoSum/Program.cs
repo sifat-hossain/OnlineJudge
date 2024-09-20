1eda42ed08a25917cfe125c1acf980ecf8e49bdb
Sifat Hossain <sifat1258@gmail.com>
2024-09-20 19:18:10 +0600

    common largest prefix

---
+++ LeetCode/TwoSum/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int b = 0, a = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (nums.Contains(diff))
+            {
+                b = i;
+                a = Array.IndexOf(nums, diff);
+                if (a != b)
+                {
+                    int[] finalResult = [b, a];
+                    return finalResult;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = 0;
+        n = int.Parse(Console.ReadLine());
+
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int target = int.Parse(Console.ReadLine());
+
+        Solution solution = new Solution();
+        var result = solution.TwoSum(nums, target);
+        Console.WriteLine("[" + string.Join(",", result) + "]");
+    }
+}
---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,80 @@
+﻿//public class Solution
+//{
+//    public string LongestCommonPrefix(string[] strs)
+//    {
+//        string prefix = "";
+//        if (strs.Length == 1) return strs[0];
+//        for (int i = 0; i < strs[0].Length; i++)
+//        {
+//            for (int j = 1; j < strs.Length; j++)
+//            {
+//                if (i >= strs[j].Length || strs[j][i] != strs[0][i])
+//                {
+//                    return prefix;
+//                }
+//            }
+//            prefix = prefix + strs[0][i];
+//        }
+//        return prefix;
+//    }
+//}
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var l = 0;
+        var currPrefix = strs[0];
+        while (l < strs.Length)
+        {
+            var currWord = strs[l];
+            if (currWord != currPrefix)
+            {
+                var prefix = FindPrefix(currWord, currPrefix);
+                currPrefix = prefix;
+            }
+
+            l++;
+        }
+
+        return currPrefix;
+    }
+
+    public string FindPrefix(string word, string prefix)
+    {
+        var r = prefix.Length - 1;
+        while (r >= 0)
+        {
+            if (!word.StartsWith(prefix))
+            {
+                prefix = prefix.Remove(r);
+                r--;
+            }
+            else
+            {
+                return prefix;
+            }
+
+        }
+
+        return "";
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Eneter string amouint: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] str = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            str[i] = Console.ReadLine();
+        }
+        Solution solution = new Solution();
+        Console.WriteLine(solution.LongestCommonPrefix(str));
+        Console.ReadLine();
+
+    }
+}
