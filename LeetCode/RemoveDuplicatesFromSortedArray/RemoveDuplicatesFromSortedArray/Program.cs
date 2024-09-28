c36386f459785845c62cfd5705a10c9926a21a26
Sifat Hossain <sifat1258@gmail.com>
2024-09-28 20:11:30 +0600

    Remove duplicate value from array

---
+++ LeetCode/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+
+        int value = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[value])
+            {
+                value++;
+                nums[value] = nums[i];
+            }
+        }
+
+        return value + 1;
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = 0;
+        Console.WriteLine("Enter the number:");
+        n = int.Parse(Console.ReadLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        Solution solution = new Solution();
+        Console.WriteLine(solution.RemoveDuplicates(nums));
+        Console.ReadLine();
+    }
+}
